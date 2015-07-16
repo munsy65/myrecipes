7952a21ae4268475842576568f015597ffc17d96
mark <munsy65@hotmail.com>
2015-07-16 21:17:30 +0000

    add admin fucntionality

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -3,6 +3,8 @@ class RecipesController < ApplicationController
   before_action :require_user, except: [:show, :index, :like]
   before_action :require_user_like, only: [:like]
   before_action :require_same_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  
   
   
   
@@ -66,6 +68,12 @@ class RecipesController < ApplicationController
     end
   end
   
+  def destroy
+    Recipe.find(params[:id]).destroy
+    flash[:success] = "Recipe Deleted"
+    redirect_to recipes_path
+  end
+  
   private
   
   
@@ -80,17 +88,23 @@ class RecipesController < ApplicationController
     end
     
     def require_same_user
-      if current_user != @recipe.chef
+      if current_user != @recipe.chef and !current_user.admin?
         flash[:danger] = "You can only edit your own recipes"
         redirect_to recipes_path
       end
     end
     
     def require_user_like
-    if !logged_in?
-      flash[:danger] = "You must be logged in to perform that action"
-      redirect_to :back
+      if !logged_in?
+        flash[:danger] = "You must be logged in to perform that action"
+        redirect_to :back
+      end
     end
-  end
+    
+    def admin_user
+      redirect_to recipes_path unless current_user.admin?
+        
+      
+    end
     
 end
